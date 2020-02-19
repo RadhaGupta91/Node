5a52fb0133d2a0bc0e1822e8cd86334d418857aa
radha <radha94646@gmail.com>
2020-02-19 12:44:16 +0530

    Express REST API with MOngoDB and Mongoose

--- 4. expressServer/index.js
+++ 4. expressServer/index.js
@@ -1,7 +1,9 @@
-const express = require('express'),
+const express = require('express');
 const http = require('http');
+
 const hostname = 'localhost';
 const port = 3000;
+
 const app = express();
 app.use((req, res) => {
   console.log(req.headers);
@@ -9,7 +11,9 @@ app.use((req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
+
 const server = http.createServer(app);
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
